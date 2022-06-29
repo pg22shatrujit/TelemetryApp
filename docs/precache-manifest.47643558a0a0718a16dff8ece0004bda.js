80d84426e2c5c4cf496f5d5346e662324214c97a
pg22shatrujit@vfs.com <pg22shatrujit@vfs.com>
2022-06-29 11:20:56 -0700

    Build output

---
+++ docs/precache-manifest.47643558a0a0718a16dff8ece0004bda.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f0fd3d5b8a0c72c93e",
+    "url": "/css/app.3e6d8e7d.css"
+  },
+  {
+    "revision": "3a79cf19f40eb8f6b274054b02305aec",
+    "url": "/css/media.css"
+  },
+  {
+    "revision": "e390ea4e07fbe17c5d271e38fbb8ba56",
+    "url": "/css/style-base.css"
+  },
+  {
+    "revision": "79ac9743b6fff790b901b3e12acc7b66",
+    "url": "/css/style-grid.css"
+  },
+  {
+    "revision": "64537337291c7503d5cf75abf4474a71",
+    "url": "/css/style-menu.css"
+  },
+  {
+    "revision": "9a2be61d02dbd882c8a92b6fa60d5e91",
+    "url": "/img/vfs_logo.9a2be61d.png"
+  },
+  {
+    "revision": "eb128da16d8f3a7cf395274bc3129748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f0fd3d5b8a0c72c93e",
+    "url": "/js/app.968c273d.js"
+  },
+  {
+    "revision": "2cb3e6bbbf7515f7cc0f",
+    "url": "/js/chunk-vendors.fe571987.js"
+  },
+  {
+    "revision": "b2faedf5784d9948a56a6329179d647f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
