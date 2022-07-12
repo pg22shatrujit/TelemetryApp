1d9c9a4f4a6af60c3598f52e134836636a6eeff3
pg22shatrujit@vfs.com <pg22shatrujit@vfs.com>
2022-07-12 11:34:46 -0700

    Data classes to store telemetry data (two copies to get around export default / module.exports issue)

---
+++ server/tData.js
@@ -0,0 +1,49 @@
+// Copyright (C) Shatrujit Aditya Kumar 2022, All Rights Reserved
+
+const rec = {
+    id: null,                                // auto generated unique identifier
+    version: null,                 // String from date - auto generated
+    sessionID: null,                          // Session within version
+    eventID: null,                           // Type of event
+    /*
+    location: { X: 0, Y: 0, Z: 0 },        // Event location
+    mapName: "base_map",                   // Current level
+    actor: {
+        state: 0,                          // Actor ID within session
+        id: 0,                             // Animation state
+        health: 100,                       // Current health
+        damageDone: 0,                     // Damage dealt so far
+        weapon: 0,                         // Weapon type
+        velocity: { X: 0, Y: 0, Z: 0 },    // Current speed and direction 
+        lookingAt: { X: 0, Y: 0, Z: 0 },   // Current look direction (normalized)
+        spawnAt: 0,                        // Spawn point ID (ENUM)
+        travelled: 0                       // Distance travelled so far
+    }
+    */
+}
+
+class TData {
+    constructor( serializedObj = undefined ) {
+
+        this.record = {
+            ...rec
+        }
+        if(serializedObj) {
+            this.record = {
+                ...rec,
+                ...JSON.parse(serializedObj)
+            }
+        }
+    }
+
+    get id() { return this.record.id }
+
+    serialize() {
+        return JSON.stringify( this.record )
+    }
+}
+
+module.exports = {
+    TData,
+    rec
+}
