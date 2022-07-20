75436b5b40c30a9e0f08a42459a7d4d5ed3170fe
pg22shatrujit@vfs.com <pg22shatrujit@vfs.com>
2022-07-20 12:11:00 -0700

    Collapsed two copies of TData into one

--- server/tData.js
+++ server/tData.js
@@ -1,12 +1,15 @@
 // Copyright (C) Shatrujit Aditya Kumar 2022, All Rights Reserved
 
-// Two copies of this class to get around the `export` - `module.exports` conflict
-// Server-side copy uses `module.exports`
+// Auto genereate version from date
+let currentVersion = () => {
+    return new Date().toISOString().split('T')[0]
+}
+
 const rec = {
-    id: null,                              // auto generated unique identifier
-    version: null,                         // String from date - auto generated
-    sessionID: null,                       // Session within version
-    eventID: null,                         // Type of event
+    id: -1,                                // auto generated unique identifier
+    version: currentVersion(),             // String from date - auto generated
+    sessionID: 0,                          // Session within version
+    eventID: 0,                            // Type of event
     /*
     location: { X: 0, Y: 0, Z: 0 },        // Event location
     mapName: "base_map",                   // Current level
@@ -25,13 +28,13 @@ const rec = {
 }
 
 // Class to store data and serialize
-class TData {
+export default class TData {
     constructor( serializedObj = undefined ) {
 
         this.record = {
             ...rec
         }
-
+        
         // Update record data if an object is passed to the constructor
         if(serializedObj) {
             this.record = {
@@ -41,12 +44,18 @@ class TData {
         }
     }
 
-    // Server only needs to access id
+    // Getters/Setters to read/write data
     get id() { return this.record.id }
+    get version() { return this.record.version }
+    get sessionID() { return this.record.sessionID }
+    get eventID() { return this.record.eventID }
+
+    set id(val) { this.record.id = val }
+    set version(val) { this.record.version = val }
+    set sessionID(val) { this.record.sessionID = val }
+    set eventID(val) { this.record.eventID = val }
 
     serialize() {
         return JSON.stringify( this.record )
     }
 }
-
-module.exports = TData
