49b9e049f4f362779ae136cd832e0efee3130970
Shatru <pg22shatrujit@vfs.com>
2022-08-19 14:37:51 -0700

    Commecnted out acion summary edge

--- server/tRoute.js
+++ server/tRoute.js
@@ -66,17 +66,17 @@ Router.delete('/single/:id', ( request, response, next ) => {
     next()
 })
 
-Router.get('/actionSummary/:id/:sessionId', ( request, response, next ) => {
+// Router.get('/actionSummary/:id/:sessionId', ( request, response, next ) => {
     
-    const data = [
-        ['Year', 'Sales', 'Expenses', 'Profit'],
-        ['2014', 1000, 400, 200],
-        ['2015', 1170, 460, 250],
-        ['2016', 660, 1120, 300],
-        ['2017', 1030, 540, 350]
-    ]
-    response.send(resultOK(data))
-    next()
-})
+//     const data = [
+//         ['Year', 'Sales', 'Expenses', 'Profit'],
+//         ['2014', 1000, 400, 200],
+//         ['2015', 1170, 460, 250],
+//         ['2016', 660, 1120, 300],
+//         ['2017', 1030, 540, 350]
+//     ]
+//     response.send(resultOK(data))
+//     next()
+// })
 
 module.exports = Router
