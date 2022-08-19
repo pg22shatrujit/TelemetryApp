c8377be1103dbd4065d1b7da32baf5a4e58e46c0
Shatru <pg22shatrujit@vfs.com>
2022-08-19 12:55:03 -0700

    Aggregations work on emulator, added aggeregation for location data and modularised functions

--- functions/index.js
+++ functions/index.js
@@ -1,59 +1,174 @@
 require = require("esm")(module)
 const functions = require("firebase-functions")
-const { doc, runTransaction, getDocs, addDoc, collection} = require("@firebase/firestore")
+const { doc, runTransaction, getDocs, addDoc, collection, deleteField } = require("@firebase/firestore")
 const db = require("./FirestoreSetup.js").db
 
+// Collection names for PlayerState and Location aggregations respectively
 const STATE_AGGREGATE = "PlayerState"
+const LOCATION_AGGREGATE = "Location"
 
-exports.aggregatePlayerStates = functions.firestore
-    .document( 'Telemetry/{recordID}' )
-    .onWrite(async (change, context) => {
+const STATE_KEY = "playerState"
+const LOCATION_KEY = "location"
+
+// Check two location objects for equality, Object structure => { X: 0.00, Y: 0.00 }
+let isEqualLocation = (locA, locB) => {
+
+    // Either object doesn't exist
+    if(!locA || !locB) return false;
+
+    // Either object is missing a coordinate
+    if(locA.X == undefined || locA.Y == undefined || locB.X == undefined || locB.Y == undefined) return false;
+
+    // Either coordinate of both objects aren't equal
+    if(locA.X != locB.X || locA.Y != locB.Y) return false;
+
+    // If we're still here, the values are the same
+    return true;
+}
+
+// Get key value from a snapshot change object, defaulting to null
+let getKeyData = ( change, key, isBeforeChange = false ) => {
+
+    if( isBeforeChange ) return change.before.exists ? change.before.data()[ key ] : null
+    return change.after.exists ? change.after.data()[ key ] : null
+}
+
+// Get a reference to the first document in a collection, creating one if the collection is empty
+let getAggregationReference = async ( aggregationCollection ) => {
+
+    const snapshot = await getDocs( collection( db, aggregationCollection ))
+    let aggregationsRef = null
+
+    if( !snapshot.size ) {
+        // Create doc if one doesn't exist and get the reference
+        aggregationsRef = await addDoc( collection( db, aggregationCollection ), {} )
+    }
+    else {
+        // If the collection isn't empty, grab the first doc (Always has only one doc)
+        aggregationsRef = await doc( db, aggregationCollection, snapshot.docs[0].id );
+    }
+
+    return aggregationsRef
+}
+
+let updateStateAggegation = ( aggregation, after, before ) => {
+    // Increase the count for the new state if it exists in the aggregation (Set it to one if it doesn't)
+    if( after != null) {
+        if( !aggregation.hasOwnProperty( `${ after }` ) ) aggregation[ after ] = 0
+        aggregation[ after ]++
+    }
+
+    // Decrease the count for the old state if it exists in the aggregation (Do nothing if the key doesn't exist)
+    if( before != null) {
+        // Check for key and make sure the count is a positive number
+        if( aggregation.hasOwnProperty( `${ before }` ) ) {
+            aggregation[ before ]--
+            if( aggregation[ before ] <= 0 ) {
+                aggregation[ before ] = deleteField()
+            }
+        }
+    }
+}
+
+let runStatesTransaction = async ( change ) => {
+    await runTransaction( db, async ( transaction ) => {
 
         // Get values of current and previous states if they exist
-        const newState = change.after.exists ? change.after.data().playerState : null
-        const oldState = change.before.exists ? change.before.data().playerState : null
+        const newState = getKeyData( change, STATE_KEY )
+        const oldState = getKeyData( change, STATE_KEY, true )
 
         // Break out if nothing changed
         if( newState == oldState ) return
 
         // Get a reference to the state aggregations doc
+        let aggregationsRef = await getAggregationReference( STATE_AGGREGATE )
+        if( !aggregationsRef ) return   
+
+        const aggregationsDoc = await transaction.get( aggregationsRef );
+        let aggregateStates = aggregationsDoc.data()
+
+        updateStateAggegation( aggregateStates, newState, oldState )
+
+        // Update the doc
+        transaction.update( aggregationsRef, aggregateStates );
+    });
+}
+
+let updateLocationAggregation = ( aggregation, after, before ) => {
+
+        // Location exists and has X, Y values
+        if( after != null && after.X != undefined && after.Y != undefined ) {
+            
+            let xIndex = Math.round( after.X )
+            let yIndex = Math.round( after.Y )
+
+            if( !aggregation.hasOwnProperty( `${ xIndex }` ) ) aggregation[ xIndex ] = {}
+            if( !aggregation[ xIndex ].hasOwnProperty( `${ yIndex }` ) ) aggregation[ xIndex ][ yIndex ] = 0
+            ++aggregation[ xIndex ][ yIndex ]
+        }
+
+        // Location exists and has X, Y values
+        if( before != null && before.X != undefined && before.Y != undefined ) {
+            
+            let xIndex = Math.round( before.X )
+            let yIndex = Math.round( before.Y )
+
+            if( aggregation.hasOwnProperty( `${ xIndex }` ) ) {
+
+                if( aggregation[ xIndex ].hasOwnProperty( `${ yIndex }` ) ) {
+
+                    // If both indexes exist, decrement the count for the old value
+                    aggregation[ xIndex ][ yIndex ]--
+
+                    // Remove the key if the count is 0 or less
+                    if( aggregation[ xIndex ][ yIndex ] <= 0 ) {
+                        delete aggregation[ xIndex ][ yIndex ]
+                    }
+                }
+                
+                // Check size of the xIndex and delete if its empty 
+                if( Object.keys( aggregation[ xIndex ] ).length == 0  ) aggregation[ xIndex ] = deleteField()
+            }
+        }
+}
+
+let runLocationsTransaction = async ( change ) => {
+    await runTransaction( db, async ( transaction ) => {
+
+        // Get values of current and previous states if they exist
+        const newLocation = getKeyData( change, "location" )
+        const oldLocation = getKeyData( change, "location", true )
+        
+        // Break out if nothing changed
+        if( isEqualLocation( newLocation, oldLocation ) ) return
+
+        // Get a reference to the state aggregations doc
+        let aggregationsRef = await getAggregationReference( LOCATION_AGGREGATE )
+        if( !aggregationsRef ) return
+
+        const aggregationsDoc = await transaction.get( aggregationsRef );
+        let aggregateLocations = aggregationsDoc.data()
+
+        updateLocationAggregation( aggregateLocations, newLocation, oldLocation )
+
+        // Update the doc
+        transaction.update( aggregationsRef, aggregateLocations );
+    })
+}
+
+// Update necessary collections everytime a document in the telemetry collection gets updated
+exports.aggregateTelemetryData = functions.firestore
+    .document( 'Telemetry/{recordID}' )
+    .onWrite(async (change, context) => {
+
+        let aggregationTransactions = []
 
         // Update aggregations in a transaction
-        await runTransaction( db, async ( transaction ) => {
+        aggregationTransactions.push( runStatesTransaction( change ) )
+        aggregationTransactions.push( runLocationsTransaction( change ) )
 
-            const snapshot = await getDocs( collection( db, STATE_AGGREGATE ))
-            let aggregationsRef = null
-
-            if( !snapshot.size ) {
-                // Create doc if one doesn't exist and get the reference
-                aggregationsRef = await addDoc( collection( db, STATE_AGGREGATE ), {} )
-            }
-            else {
-                // If the collection isn't empty, grab the first doc (Always has only one doc)
-                aggregationsRef = await doc( db, STATE_AGGREGATE, snapshot.docs[0].id );
-            }
-
-            if( !aggregationsRef ) return
-
-            const aggregationsDoc = await transaction.get( aggregationsRef );
-            let aggregateStates = aggregationsDoc.data()
-
-            // Decrease the count for the old state if it exists in the aggregation (Do nothing if the key doesn't exist)
-            if( oldState != null) {
-                // Check for key and make sure the count is a positive number
-                if( aggregateStates.hasOwnProperty( `${ oldState }` ) && aggregateStates[ oldState ] > 0 ) aggregateStates[ oldState ]--
-            }
-
-            // Increase the count for the new state if it exists in the aggregation (Set it to one if it doesn't)
-            if( newState != null) {
-                if( aggregateStates.hasOwnProperty( `${ newState }` ) ) aggregateStates[ newState ]++
-                else aggregateStates[ newState ] = 1
-            }
-
-            // Update the doc
-            transaction.update(aggregationsRef, aggregateStates);
-        });
-    });
+        await Promise.all(aggregationTransactions)
+    })
 
 // class TelemetryFunctions {
 
@@ -80,11 +195,3 @@ exports.aggregatePlayerStates = functions.firestore
 
 // const telemetryfunctions = new TelemetryFunctions()
 // exports = telemetryfunctions.exports
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
